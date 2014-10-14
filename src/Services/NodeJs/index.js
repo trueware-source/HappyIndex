64091ad9ca6014ed5aabc07d7c53ac3678139c44
Terrence Benade <terrence.benade@gmail.com>
2014-10-14 23:59:07 +1100

    getting hapi

---
+++ src/Services/NodeJs/index.js
@@ -0,0 +1,25 @@
+﻿var Hapi = require('hapi');
+var Good = require('good');
+
+// Create a server with a host and port
+var server = new Hapi.Server('localhost', 8000);
+
+// Add the route
+server.route({
+  method: 'GET',
+  path: '/api',
+  handler: function (request, reply) {
+
+    reply('hello world');
+  }
+});
+
+server.pack.register(Good, function (err) {
+  if (err) {
+    throw err; // something bad happened loading the plugin
+  }
+
+  server.start(function () {
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
+});
